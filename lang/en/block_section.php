3021642d630179ba36038b253aad309c9c8f0d39
Arnaud Trouvé <arnaud.trouve@andil.fr>
2020-01-07 16:52:24 +0100

    Add missing string

--- lang/en/block_section.php
+++ lang/en/block_section.php
@@ -32,4 +32,5 @@ $string['blocktitle'] = 'Title';
 $string['blocktitle_help'] = 'The title for this instance of the block. Appears in the block header.';
 $string['blockteacheredit'] = 'Teachers Can Edit This Block';
 $string['section:addinstance'] = 'Add a Section block';
+$string['section:myaddinstance'] = 'Add a Section block to Dashboard';
 $string['privacy:metadata'] = 'The Section block plugin does not store any personal data.';
